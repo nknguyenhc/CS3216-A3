94bcb1e3d2dc210edaf64635042bcd9b6535a4b0
ARPspoofing <mohdreyaaz99@gmail.com>
2024-09-14 22:56:32 +0800

    Add video

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -2,7 +2,7 @@ import Hero from "../components/Hero";
 import Pricing from "../components/Pricing/Pricing";
 import Footer from "../components/Footer";
 import Accordion from "../components/Accordions/Accordion";
-import AnimateOnScroll from "../components/Features/AnimateOnScroll";
+import ScrollVideo from "../components/Features/ScrollVideo";
 import FeedbackSteps from "../components/Steps/FeedbackSteps";
 import AdvantagesSection from "../components/Advantage/AdvantageSection";
 import ScrollAnimation from "../components/Features/ScrollAnimation";
@@ -12,10 +12,10 @@ const LandingPage = () => {
     <div>
       <Hero />
       <AdvantagesSection />
+      <ScrollVideo />
       <FeedbackSteps />
       <Pricing />
       <Accordion />
-      <AnimateOnScroll />
       <ScrollAnimation/>
 
       
