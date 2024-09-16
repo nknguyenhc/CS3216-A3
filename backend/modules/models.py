696b39b15d2ca75ba236d37cc7df3d55c5af0072
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-09-16 09:37:05 +0800

    Complete suitability
    * Add capability
    * Add string representation of models

--- backend/modules/models.py
+++ backend/modules/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+import json
 
 
 class PersonalStatement(models.Model):
     field_of_study = models.TextField()
     essay = models.TextField()
     reparagraphed_essay = models.TextField(null=True)
+
+    def __str__(self):
+        return json.dumps({
+            "field_of_study": self.field_of_study,
+            "essay": self.essay,
+            "reparagraphed_essay": self.reparagraphed_essay,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Argument(models.Model):
@@ -14,17 +25,45 @@ class Argument(models.Model):
     personal_statement = models.ForeignKey(
         PersonalStatement, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return json.dumps({
+            "idea": self.idea,
+            "evidence": self.evidence,
+            "explanation": self.explanation,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Specificity(models.Model):
     is_specific = models.BooleanField()
     reason = models.TextField()
     argument = models.ForeignKey(Argument, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return json.dumps({
+            "is_specific": self.is_specific,
+            "reason": self.reason,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Relevance(models.Model):
     is_relevant = models.BooleanField()
     reason = models.TextField()
     argument = models.ForeignKey(Argument, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return json.dumps({
+            "is_relevant": self.is_relevant,
+            "reason": self.reason,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Suitability(models.Model):
@@ -34,10 +73,29 @@ class Suitability(models.Model):
     is_capable_reason = models.TextField()
     argument = models.ForeignKey(Argument, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return json.dumps({
+            "has_interest": self.has_interest,
+            "has_interest_reason": self.has_interest_reason,
+            "is_capable": self.is_capable,
+            "is_capable_reason": self.is_capable_reason,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 class Comment(models.Model):
     comment = models.TextField()
     argument = models.ForeignKey(Argument, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return json.dumps({
+            "comment": self.comment,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class ArgumentEvaluations(models.Model):
@@ -45,6 +103,16 @@ class ArgumentEvaluations(models.Model):
     specificity = models.ForeignKey(Specificity, on_delete=models.CASCADE)
     relevance = models.ForeignKey(Relevance, on_delete=models.CASCADE)
     suitability = models.ForeignKey(Suitability, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return json.dumps({
+            "specificity": self.specificity,
+            "relevance": self.relevance,
+            "suitability": self.suitability,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
 
     class Meta:
         managed = False
@@ -54,3 +122,11 @@ class GeneralComment(models.Model):
     comment = models.TextField()
     personal_state = models.ForeignKey(
         PersonalStatement, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return json.dumps({
+            "comment": self.comment,
+        }, indent=4)
+
+    def __repr__(self):
+        return self.__str__()
--- backend/modules/modules/suitability.py
+++ backend/modules/modules/suitability.py
@@ -1,5 +1,6 @@
 from modules.models import Argument, Suitability
 from openai import OpenAI
+import logging
 
 
 class SuitabilityIdentifier:
@@ -21,12 +22,41 @@ class SuitabilityIdentifier:
         with open(capability_user_prompt_path, "r") as f:
             self.capability_user_prompt = f.read()
 
+        self.logger = logging.getLogger("SuitabilityIdentifier")
+
     def identify_suitability(self, argument: Argument) -> Suitability:
         """
         Identifies the suitability of an argument.
         Writes the result to database.
         """
-        return self._identify_interest(argument)
+        try:
+            interest_result = self._identify_interest(argument)
+            has_interest, has_interest_reason = self._parse_result(
+                interest_result)
+        except Exception as e:
+            self.logger.exception("Failed to identify interest")
+            return None
+
+        try:
+            capability_result = self._identify_capability(argument)
+            is_capable, is_capable_reason = self._parse_result(
+                capability_result)
+        except Exception as e:
+            self.logger.exception("Failed to identify capability")
+            return None
+
+        try:
+            suitability = Suitability.objects.create(
+                argument=argument,
+                has_interest=has_interest,
+                has_interest_reason=has_interest_reason,
+                is_capable=is_capable,
+                is_capable_reason=is_capable_reason,
+            )
+            return suitability
+        except Exception as e:
+            self.logger.exception("Failed to save suitability")
+            return None
 
     def _identify_interest(self, argument: Argument) -> str:
         client = OpenAI()
@@ -44,3 +74,29 @@ class SuitabilityIdentifier:
             ]
         )
         return completion.choices[0].message.content
+
+    def _identify_capability(self, argument: Argument) -> str:
+        client = OpenAI()
+        completion = client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": self.capability_system_prompt.format(
+                    field_of_study=argument.personal_statement.field_of_study,
+                )},
+                {"role": "user", "content": self.capability_user_prompt.format(
+                    idea=argument.idea,
+                    evidence=argument.evidence,
+                    explanation=argument.explanation,
+                )},
+            ]
+        )
+        return completion.choices[0].message.content
+
+    def _parse_result(self, result: str) -> tuple[bool, str]:
+        letter = result[0]
+        if letter == "T":
+            return True, result[2:]
+        elif letter == "F":
+            return False, result[2:]
+        else:
+            raise ValueError(f"Invalid result: {result}")
