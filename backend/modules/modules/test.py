8a33092ab975ad2e752d0e293efbe5d440f919ae
ChauuuuLe <lehuychau130105@gmail.com>
2024-09-18 16:54:21 +0700

    relevance checking feature

--- backend/modules/modules/test.py
+++ backend/modules/modules/test.py
@@ -10,6 +10,7 @@ if __name__ == '__main__':
     from .interest import InterestIdentifier
     from .capability import CapabilityIdentifier
     from .reparagraph import ReParagrapher
+    from .relevance import RelevanceIdentifier
     from modules.models import Argument, PersonalStatement
     import json
 
@@ -60,12 +61,25 @@ if __name__ == '__main__':
             essay=data["essay"],
             reparagraphed_essay=data["reparagraphed_essay"]
         )
+    with open("modules/modules/tests/example_relevance/example5.json") as f:
+        data = json.load(f)
+        example5 = Argument(
+            idea=data["idea"],
+            evidence=data["evidence"],
+            explanation=data["explanation"],
+            personal_statement=PersonalStatement(
+                field_of_study=data["field_of_study"],
+            ),
+        )
 
-    suitability_result = CapabilityIdentifier().identify_capability(example2)
-    print(suitability_result)
-    interest_result = InterestIdentifier().identify_interest(example2)
-    print(interest_result)
+    #suitability_result = CapabilityIdentifier().identify_capability(example2)
+    #print(suitability_result)
+    #interest_result = InterestIdentifier().identify_interest(example2)
+    #print(interest_result)
 
     # has_conclusion, reparagraph_result = ReParagrapher().reparagraph(example_ps)
     # print(has_conclusion)
     # print(reparagraph_result)
+
+    relevance = RelevanceIdentifier().identify_relevance(example5)
+    print(relevance)
