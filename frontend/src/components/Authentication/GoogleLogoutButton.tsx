729d6bc1d87f0bc5acf88060bb8b1ed270b94327
ARPspoofing <mohdreyaaz99@gmail.com>
2024-09-24 23:17:51 +0800

    Add new logout

--- frontend/src/components/Authentication/GoogleLogoutButton.tsx
+++ frontend/src/components/Authentication/GoogleLogoutButton.tsx
@@ -1,3 +1,4 @@
+/*
 import { GoogleLogout } from 'react-google-login';
 
 const clientId: string = '577083967585-ofhpvr34hgknf49vacjpkpth8n2gklub.apps.googleusercontent.com';
@@ -18,5 +19,37 @@ const GoogleLogoutButton: React.FC = () => {
         </div>
     )
 }
+
+export default GoogleLogoutButton;
+*/
+
+import { googleLogout } from '@react-oauth/google';
+import { useAuth } from './AuthenticationContext';
+import { useNavigate } from 'react-router-dom';
+
+const GoogleLogoutButton: React.FC = () => {
+    const { setCurrentUser, setLoggedInWithGoogle } = useAuth();
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        setCurrentUser(false);
+        setLoggedInWithGoogle(false);
+        navigate("/");
+        console.log("Logout successful!");
+    };
+
+    const handleLogout = () => {
+        googleLogout();
+        onLogout();
+    };
+
+    return (
+        <div id="signOutButton">
+            <button onClick={handleLogout}>
+                Logout
+            </button>
+        </div>
+    );
+};
 
 export default GoogleLogoutButton;
--- frontend/src/components/Navbar/AuthButtons.tsx
+++ frontend/src/components/Navbar/AuthButtons.tsx
@@ -1,3 +1,4 @@
+/*
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../Authentication/AuthenticationContext";
 import { GoogleLogout } from "react-google-login";
@@ -67,6 +68,85 @@ const AuthButtons = () => {
               <GoogleLogout clientId={clientId} buttonText="Log Out" onLogoutSuccess={handleLogoutSuccess} />
             </div>
           )}
+          <button
+            className="gap-2.5 self-start px-4 py-3.5 text-red-600 rounded-xl border border-red-600 border-solid min-h-[50px] hover:bg-red-600 hover:text-white transition duration-200"
+            onClick={handleLogout}
+          >
+            Log Out
+          </button>
+        </>
+      ) : (
+        <button
+          className="gap-2.5 self-start px-4 py-3.5 text-sky-600 rounded-xl border border-sky-600 border-solid min-h-[50px] hover:bg-sky-600 hover:text-white transition duration-200"
+          onClick={handleAuthentication}
+        >
+          Login / Sign Up
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default AuthButtons;
+*/
+
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Authentication/AuthenticationContext";
+import { googleLogout } from "@react-oauth/google";
+import { client, getCSRFToken } from "../../AxiosInstance/AxiosInstance";
+import ReactGA from "react-ga";
+
+const AuthButtons = () => {
+  const { currentUser, setCurrentUser } = useAuth();
+  const { loggedInWithGoogle, setLoggedInWithGoogle } = useAuth();
+  const navigate = useNavigate();
+
+  const handleAuthentication = () => {
+    navigate("/authentication");
+  };
+
+  const handleLogoutSuccess = () => {
+    console.log("Google logout successful!");
+    ReactGA.event({
+      category: "User",
+      action: "Logout Successful",
+      label: "Google Logout"
+    });
+    setCurrentUser(false);
+    setLoggedInWithGoogle(false);
+    navigate("/");
+  };
+
+  const handleLogout = async () => {
+    if (loggedInWithGoogle) {
+      googleLogout();
+      handleLogoutSuccess();
+    } else {
+      const csrfToken = getCSRFToken();
+
+      try {
+        await client.post("/api/auth/logout", {}, { headers: { "X-CSRFToken": csrfToken } });
+        ReactGA.event({
+          category: "User",
+          action: "Logout Successful",
+          label: "API Logout"
+        });
+        setCurrentUser(false);
+        navigate("/");
+      } catch (error) {
+        ReactGA.event({
+          category: "User",
+          action: "Logout Failed",
+        });
+        console.error("Logout failed:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="flex gap-4 self-stretch font-bold whitespace-nowrap">
+      {currentUser ? (
+        <>
           <button
             className="gap-2.5 self-start px-4 py-3.5 text-red-600 rounded-xl border border-red-600 border-solid min-h-[50px] hover:bg-red-600 hover:text-white transition duration-200"
             onClick={handleLogout}
