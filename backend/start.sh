cc0a8db0645ae8e95d968c46c87ae85cf928c79f
nknguyenhc <87511888+nknguyenhc@users.noreply.github.com>
2024-09-26 09:33:18 +0800

    Update gunicorn command
    * Increase number of workers
    * Increase timeout

--- backend/start.sh
+++ backend/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python manage.py migrate
-gunicorn --bind 0.0.0.0:80 backend.wsgi:application
+gunicorn backend.wsgi:application --bind 0.0.0.0:80 --workers 4 --timeout 300 --log-level=debug
