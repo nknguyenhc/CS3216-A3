878a5dba0e2317c3ebe0ea69d4b1aec82ad00852
ARPspoofing <mohdreyaaz99@gmail.com>
2024-09-26 11:59:49 +0800

    Add logo nav

--- frontend/src/components/Navbar/Logo.tsx
+++ frontend/src/components/Navbar/Logo.tsx
@@ -1,11 +1,15 @@
+import { Link } from 'react-router-dom';
+
 const Logo = () => {
   return (
-    <img
-      loading="lazy"
-      src="./assets/logo-no-background.png"
-      alt="Company Logo"
-      className="object-contain shrink-0 self-stretch my-auto max-w-full aspect-[5] w-[200px] h-auto"
-    />
+    <Link to="/" className="flex">
+      <img
+        loading="lazy"
+        src="./assets/logo-no-background.png"
+        alt="Company Logo"
+        className="object-contain shrink-0 self-stretch my-auto max-w-full aspect-[5] w-[200px] h-auto"
+      />
+    </Link>
   );
 };
 
